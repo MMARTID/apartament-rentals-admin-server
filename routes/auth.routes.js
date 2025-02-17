28ebe2db1d4f76bbaa51163d55ce7290b103b60f
Victor Hugo Guardiola <victorhugo.pessoal.guardiola@gmail.com>
2025-02-17 19:02:21 +0100

    Creación de usuario

---
+++ routes/auth.routes.js
@@ -0,0 +1,61 @@
+const router = require("express").Router();
+const User = require("../models/User.model.js");
+//POST "/api/auth/signup" => registra el usuario
+router.post("/signup", async(req, res, next) => {
+
+  // recibir la data del usuario
+  console.log(req.body)
+  const { name, email, password }  = req.body;
+
+  // validaciones
+    //0. Data existe
+    if (!name || !email || !password){
+      res.status(400).json('tienes que ingresar name, email y password')
+      return
+    }
+
+    //1. name minimo 2 caracteres
+    if (name.length < 2){
+      res.status(400).json('el nombre ha de tener como minimo 2 caracteres')
+      return
+    }
+
+    //2. constraseña con nivel de seguridad
+    const regexPasswordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm
+    if( regexPasswordPattern.test(password) === false ) {
+      res.status(400).json({errorMessage: "La contraseña no es valida. Debe contener por lo menos una mayuscula, una minuscula y un numero. Puede conetener caracteres especiales"})
+      return // detener la ejecución de la ruta
+    }
+    
+    //3. correo electronico con formato correcto
+    const regexEmailPattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gm
+    if( regexEmailPattern.test(email) === false ) {
+      res.status(400).json({errorMessage: "El formato de email no es valido."})
+      return // detener la ejecución de la ruta
+    }
+
+  // crear el usuario
+  try {
+    const encontrarUser = await User.findOne({email : email})
+    
+    if (encontrarUser !== null){
+      res.status(400).json('Ya existe un usuario con este correo')
+      return
+    }
+    await User.create({
+     name : name,
+     email : email,
+     password : email
+    })
+   res.status(201).json('usuario creado ✅')
+   
+  } catch (e) {
+    next(e)
+  }
+})
+
+//POST "/api/auth/login" => autenticación del usuario y envio del token
+
+//GET "/api/auth/verify" => validación del token
+
+module.exports = router;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,9 +5,12 @@ router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
+const authRouter = require("./auth.routes")
+router.use("/auth", authRouter)
 
 const rentsRoutes = require("./rents.routes");
 router.use("/rents", rentsRoutes);
+
 const usersRoutes = require("./users.routes");
 router.use("/users", usersRoutes);
 
