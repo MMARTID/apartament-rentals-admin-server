dedb66a1defbf4c7b1750a122f6ab0336bae41e3
MMARTID <miguelmartindelgado1998@gmail.com>
2025-02-18 13:59:09 +0100

    user routes

--- routes/rents.routes.js
+++ routes/rents.routes.js
@@ -1,12 +1,34 @@
 const router = require("express").Router();
 const Rents = require("../models/Rent.model.js");
 
-// GET => /api/rents/all    
+// GET => /api/rents/all   
+//! OBTENER TODOS LOS RENTS 
 router.get("/all", async (req, res, next) => {
   const allRents = await Rents.find()
     res.json(allRents);
 });
-// GET => /api/rents 
+
+// GET => api/rents/own/all
+//!OBTENER LOS RENTS DEL USUARIO LOGUEADO (PAYLOAD)
+router.get("/own/all", async (req, res, next) => {
+  const { _id } = req.payload
+  const allRents = await Rents.find({owner: _id})
+    res.json(allRents);
+});
+
+// GET => /api/rents/user/:userId
+//! OBTENER TODOS LOS RENTS DE UN USUARIO 
+
+
+// GET => /api/rents/own/favorites
+//! OBTENER TODOS LOS RENTS FAVORITOS DEL USUARIO LOGUEADO (PAYLOAD)
+
+
+// PATCH => /api/rents/:rentId/edit
+//! EDITAR UN RENT
+
+
+// POST => /api/rents/create
 //! AÑADIR PARAMTRO DINAMICO CON EL ID DEL USUARIO QUE VA A CREAR EL RENT
 //! COMPARARLO CON EL ID DEL USUARIO QUE ESTA LOGUEADO
 router.post("/create", async (req, res, next) => {
@@ -39,4 +61,8 @@ router.post("/create", async (req, res, next) => {
     next(e);
   }
 });
+
+// DELETE => /api/rents/:rentId/delete
+//! ELIMINAR UN RENT
+
 module.exports = router;
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,36 +1,41 @@
 const router = require("express").Router();
 const User = require("../models/User.model.js");
 //rutas de usuario
-
+// GET => /api/users/all
 //! OBTEER TODOS LOS USUARIOS 
-
-//! EDITAR USUARIO
-
-//! crear usuario (provisional, tiene que ir en auth.routes)
-// GET => /api/users/create
-router.post("/create", async (req, res, next) => {
-    const {
-      name,
-      email,
-      password,
-      rents,
-      role,
-      friends
-    } = req.body;
+router.get("/all", async (req, res, next) => {
   try {
-    const newUser = await User.create({
-      name,
-      email,
-      password,
-      rents,
-      role,
-      friends
-    });
-    res.status(201).json(newUser);
+    const allUsers = await User.find()
+    res.json(allUsers);
+  } catch (e) {
+    
+  }
+});
+
+// GET => /api/users/:userId
+//! OBTENER UN USUARIO POR SU ID
+router.get("/:userId", async (req, res, next) => {
+  const { userId } = req.params;
+  try {
+    const user = await User.findById(userId)
+    res.json(user);
   } catch (e) {
     next(e);
-    res.status(500).json({ message: "Error creating user" });
-
   }
-})
+  
+});
+
+// PATCH => /api/users/own/:userId/edit
+//! EDITAR USUARIO (PAYLOAD)
+router.patch("/own/:userId/edit", async (req, res, next) => {
+  const { userId } = req.params;
+  const { name, email, password } = req.body;
+  try {
+    const user = await User.findByIdAndUpdate(userId, { name, email, password });
+    res.json(user);
+  } catch (e) {
+    next(e);
+  }
+});
+
 module.exports = router;
